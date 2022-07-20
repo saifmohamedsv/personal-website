7f8fed88177a8487fd8faf196110dd2c8ed50682
saifmohamedsv <xcyberghostx1@gmail.com>
2022-07-20 19:23:33 +0200

    last

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import {About, Footer, Header, Skills, Testimonial, Work} from './container';
 import {Navbar} from './components';
 import './App.scss';
 
-const App = () => (
-        <div className="app">
-            <Navbar/>
-            <Header/>
-            <About/>
-            <Work/>
-            <Skills/>
-            <Testimonial/>
-            <Footer/>
-        </div>
-);
+const App = () => (<div className="app">
+        <Navbar/>
+        <Helmet>
+            <script async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5117302971351802"
+                    crossOrigin="anonymous">
+
+            </script>
+        </Helmet>
+        <Header/>
+        <About/>
+        <Work/>
+        <Skills/>
+        <Testimonial/>
+        <Footer/>
+    </div>);
 
 export default App;
