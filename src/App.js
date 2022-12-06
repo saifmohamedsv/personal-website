647e7158d6311cea39f0bfa212f7f029e8e4aaf4
saifmohamedsv <xcyberghostx1@gmail.com>
2022-12-06 14:19:34 +0200

    2023 update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import {About, Footer, Header, Skills, Testimonial, Work} from './container';
-import {Navbar} from './components';
-import './App.scss';
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+import { Navbar } from "./components";
+import "./App.scss";
 
-const App = () => (<div className="app">
-        <Navbar/>
-        <Helmet>
-            <script async
-                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5117302971351802"
-                    crossOrigin="anonymous">
-
-            </script>
-        </Helmet>
-        <Header/>
-        <About/>
-        <Work/>
-        <Skills/>
-        <Testimonial/>
-        <Footer/>
-    </div>);
+const App = () => (
+  <div className="app">
+    <Navbar />
+    <Header />
+    <About />
+    <Work />
+    <Skills />
+    <Testimonial />
+    <Footer />
+  </div>
+);
 
 export default App;
